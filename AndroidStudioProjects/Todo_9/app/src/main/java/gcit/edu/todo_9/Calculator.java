f8d25ca26045256b39810bdd0e6b0c6f5960caff
Ugyenlhamo <12190102.gcit@rub.edu.bt>
2021-05-31 12:04:10 +0600

    TODO and Assignments

--- AndroidStudioProjects/Todo_9/app/src/main/java/gcit/edu/todo_9/Calculator.java
+++ AndroidStudioProjects/Todo_9/app/src/main/java/gcit/edu/todo_9/Calculator.java
@@ -3,9 +3,11 @@ package gcit.edu.todo_9;
 public class Calculator {
     public enum operator{ADD,SUB,MUL,DIV};
     public double add(double operand1,double operand2){
+
         return operand1+operand2;
     }
-    public double sub(double operand1,double operand2){
+    public double sub(double operand1,double operand2)
+    {
         return operand1-operand2;
     }
     public double mul(double operand1,double operand2){
